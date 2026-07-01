f6289dfcd4bdffd81e0638f9c417816d14587520
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-07-01 17:07:29 +0700

    Add multi queries for each decomposed question

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.load import dumps, loads 
 from langchain_core.output_parsers import JsonOutputParser
 # LLM
 gemini = ChatGoogleGenerativeAI(
@@ -67,6 +68,15 @@ decomposed questions: ["How to find authors of a scientific paper"
 """
 
 # Multi-querry 
+def get_unique_union(documents):
+    """ Unique union of retrieved docs """
+    # Flatten list of lists, and convert each Document to string
+    flattened_docs = [dumps(doc) for sublist in documents for doc in sublist]
+    # Get unique documents
+    unique_docs = list(set(flattened_docs))
+    # Return
+    return [loads(doc) for doc in unique_docs]
+
 template = """You are an AI language model assistant. Your task is to generate five 
 different versions of the given user question to retrieve relevant documents from a vector 
 database. By generating multiple perspectives on the user question, your goal is to help
@@ -74,14 +84,20 @@ the user overcome some of the limitations of the distance-based similarity searc
 Provide these alternative questions separated by newlines. Original question: {question}"""
 prompt_multiple_querries = ChatPromptTemplate.from_template(template)
 multi_querries_questions = {}
+generate_multi_querries = (
+    {"question": RunnablePassthrough()}
+    | prompt_multiple_querries
+    | gemini
+    | StrOutputParser()
+)
 for idx, ques in enumerate(decomposed_questions):
-    generate_multi_querries = (
-        {"question": RunnablePassthrough()}
-        | prompt_multiple_querries
-        | gemini
-        | StrOutputParser()
-    )
-    multi_querries_questions[idx] = generate_queries_decomposition.invoke(ques)
+    multi_querries_questions[idx] = generate_multi_querries.invoke(ques)
+retrieved_docs = {}
+
+for idx, queries in multi_querries_questions.items():
+    docs = retriver.map().invoke(queries)
+    retrieved_docs[idx] = get_unique_union(docs)
+    
 """
 print(f"Multi querries; {multi_querries_questions}")
 Multi querries; {
@@ -90,14 +106,12 @@ Multi querries; {
     2: ['List of academic databases for scientific publications', 'How to find authors using scientific publication databases', 'Best databases for tracking scientific authors and their publications']}
 """
 
-# Normal RAG
-template = """Answer the following question based on this context:
-
-{context}
-
-Question: {question}
-"""
-prompt_normal_rag = ChatPromptTemplate.from_template(template)
+# Fusion based on multi-queries context
 
 
-    
+# Decomposed questions answer based on multi queries context
+
+
+# Format q&a pairs (decomposed questions) and final answer
+
+
